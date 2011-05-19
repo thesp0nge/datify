4996da8be573653c9afe323fe069af79ab6fcd21
Paolo Perego <thesp0nge@gmail.com>
2011-05-19 17:27:49 +0200

    Test suite is green. Basic controls is here

--- lib/datify/engine.rb
+++ lib/datify/engine.rb
@@ -3,11 +3,15 @@ require 'datify/language'
 module Datify
   class Engine
     include Datify::Language
+    attr_reader :date
 
     def initialize(string = nil)
       default_language
       @array = parse_string(string)
-      
+    end
+
+    def date
+      (self.valid?)? @date = Time.mktime(@array[2], get_month(@array[1]), @array[0]) : nil
     end
 
     def parse_string(string)
@@ -15,7 +19,7 @@ module Datify
     end
 
     def valid?
-      (@array.size == 3)
+      ((@array.size == 3) && (get_month(@array[1]) != @array[1]))
     end
   end
 end
--- lib/datify/language.rb
+++ lib/datify/language.rb
@@ -2,6 +2,8 @@ module Datify
   module Language
     attr_reader :lang
 
+    MONTHS_IT = { 'GEN' => 1, 'FEB' => 2, 'MAR' => 3, 'APR' => 4, 'MAG' => 5, 'GIU' => 6, 'LUG' => 7, 'AGO' => 8, 'SET' => 9, 'OTT' => 10, 'NOV' => 11, 'DIC' => 12, }
+    
     def default_language
       @lang = 'it'
     end
@@ -10,6 +12,10 @@ module Datify
       @lang = locale
     end
 
+    def get_month(m)
+      MONTHS_IT[m.upcase] || m
+    end
+    
     
   end
 end
