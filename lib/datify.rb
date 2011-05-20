009c9d881a19aa0684aad9e2b384a5efc8dace62
Paolo Perego <thesp0nge@gmail.com>
2011-05-20 09:10:56 +0200

    Version bump to 0.50 Now the String class has been modified with two additional methods taking care about giving a Time object and a boolean saying your string is a good time or not.

--- lib/datify.rb
+++ lib/datify.rb
@@ -1,1 +1,11 @@
 require 'datify/engine'
+
+class String
+  def is_time?
+    Datify::Engine.new(self).valid?
+  end
+
+  def to_time
+    Datify::Engine.new(self).date
+  end
+end
---
+++ spec/string_spec.rb
@@ -0,0 +1,23 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe String, "datify" do
+  it "should have the to_time method" do
+    "A string".methods.index('to_time'.to_sym).should_not be_nil
+  end
+
+  it "should have the is_time? method" do
+    "A string".methods.index('is_time?'.to_sym).should_not be_nil
+  end
+
+  it "should return a valid Time object for valid dates" do
+    "23-giu-1973".is_time?.should be_true
+    "23-giu-1973".to_time.should be_a(Time)
+  end
+  it "should tell an invalid date is invalid" do
+    "This is not a date".is_time?.should be_false
+  end
+
+  it "should return a nil object when trying to convert an invalid date" do
+    "This is not a date".to_time.should be_nil
+  end
+end
